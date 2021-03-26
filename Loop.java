af939a2229450ac452b0824d268a6ee148fcf117
rogerh3 <81320876+rogerh3@users.noreply.github.com>
2021-03-26 13:15:21 -0400

    Add files via upload

---
+++ Loop.java
@@ -0,0 +1,50 @@
+//import java.until.Scanner;
+
+
+public class Loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		int number = 5;
+		
+//		number = number + 1; 
+		// same as number++
+		
+//		number = number - 1;
+		// same as number--
+		
+//		int x = 10;
+		
+//		while (x > 0) {
+//			System.out.println(x);
+//			x--; 
+//		}
+		
+//		Scanner keyboard = new Scanner(System.in); 
+//		
+//		System.out.println("Pick a number from 1-100");
+//		int choice = keyboard.nextInt(); 
+//		
+//		while (choice < 1 || choice > 100) {
+//			System.out.println("Number is invalid. Please pick another number.");
+//			choice = keyboard.nextInt(); 
+//			
+//		}
+
+		
+//		int x = 1;
+		
+//		do {
+//			System.out.println(x);
+//				x++;  
+//		}while(x < 10); 
+
+		
+		for (int i = 0; i<10; i++) {
+			System.out.println(i);
+		}
+			
+	}
+	
+}
